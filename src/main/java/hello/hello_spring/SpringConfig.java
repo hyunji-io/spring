00106fbada3369673bd066c292396fa51175e6f5
hyunji <hyunji0720@icloud.com>
2024-12-25 09:13:29 +0900

    Feat: JdbcMemberRepository를 통해 DB 접근 구현

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello.hello_spring;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+	// 스프링이 자체적으로 관리
+	private final DataSource dataSource;
+
+	@Autowired
+	public SpringConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	@Bean
 	public MemberService memberService() {
@@ -17,6 +28,6 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
